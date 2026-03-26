5e21654877630ac155347013ba9f38bace609576
Jha-Yogita <yogitajha5@gmail.com>
2026-03-26 20:24:20 +0530

    deployment

--- server.py
+++ server.py
@@ -113,4 +113,5 @@ def status():
 
 
 if __name__ == "__main__":
-    app.run(debug=False, port=8000, host="0.0.0.0")
+    port = int(os.environ.get("PORT", 7860))
+    app.run(host="0.0.0.0", port=port)
